81215c59d303d25bec0b7cea3fd8d28a5fa76e36
edw-perzd <legna246.dav@gmail.com>
2023-12-05 20:34:07 -0600

    03 Se agrega Nave y movimiento de la misma con Canvas

--- navecita.js
+++ navecita.js
@@ -8,9 +8,25 @@ let contador = 0;
 let vidas = 3;
 let pointMax = leerPointMax();
 
+let CorX = 430;
+let CorY = 440;
+
+let position = 0;
+
 puntaje.innerHTML = contador;
 pmax.innerHTML = pointMax;
 vidasT.innerHTML = crearVidas(vidas);
+
+function nave(x, y){
+    ctx.lineWidth = 3;
+    ctx.strokeStyle = 'white';
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+    ctx.lineTo(x + 20, y - 40);
+    ctx.lineTo(x + 40, y);
+    ctx.stroke();
+    
+}
 
 function leerPointMax(){
     const points = 0;
@@ -27,3 +43,35 @@ function crearVidas(vidas){
     }
     return spanVidas;
 }
+
+setInterval(() =>
+{
+    nave(CorX, CorY);
+    if(position === 1){
+        if(CorX > 5){
+            ctx.clearRect(0, 0, 900, 450);
+            CorX -=5;
+            position = 0;
+        }
+    }
+    else if(position === 2){
+        if(CorX < 850){
+            ctx.clearRect(0, 0, 900, 450);
+            CorX +=5;
+            position = 0;
+        }
+    }
+    
+},2)
+
+document.querySelector('body').addEventListener('keydown', (e) => {
+    e.preventDefault();
+    switch(e.key){
+        case 'ArrowLeft':
+            position = 1;
+            break;
+        case 'ArrowRight':
+            position = 2;
+            break;
+    }
+});
